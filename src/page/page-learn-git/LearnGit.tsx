b2d9f9db217487ca7805bfa440f994a7f75965b6
Phan Dang Vui <vuipd@vng.com.vn>
2022-07-21 13:06:06 +0700

    update thay doi lan 3

--- src/page/page-learn-git/LearnGit.tsx
+++ src/page/page-learn-git/LearnGit.tsx
@@ -5,6 +5,7 @@ export const LearnGit: React.FC<{  }> = props => {
         <div>
             <h1>Learn Git</h1>
             <h6>Thay doi Tag</h6>
+            <h6>Thay doi lan 3</h6>
         </div>
     );
 };
